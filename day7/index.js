0f724593d021cdcde871a1af9c93756ce2333731
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2024-02-14 00:09:10 +0530

    progess bar css is not added

---
+++ day7/index.js
@@ -0,0 +1,13 @@
+ function ProgressFunAdd(){
+    const Progress=document.getElementById('curState');
+    Progress.value=Progress.value+25;
+    console.log(Progress.value);
+
+ }
+
+
+function ProgressFunSub(){
+    const Progress=document.getElementById('curState')
+    Progress.value=Progress.value-25;
+    console.log(Progress.value);
+ }
