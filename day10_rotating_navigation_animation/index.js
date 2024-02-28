05ea8d3d7a221bf588d2d6665b576035f9a33449
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2024-02-28 15:16:36 +0530

    '.

---
+++ day10_rotating_navigation_animation/index.js
@@ -0,0 +1,15 @@
+const open=document.getElementById('open');
+const close=document.getElementById('close');
+const conatiner=document.querySelector('.container')
+
+
+
+open.addEventListener('click',()=>{
+    conatiner.classList.add('show-nav')
+})
+
+
+
+close.addEventListener('click',()=>{
+    conatiner.classList.remove('show-nav')
+})
